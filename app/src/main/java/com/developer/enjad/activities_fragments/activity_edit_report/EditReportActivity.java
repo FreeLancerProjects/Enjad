766479e9bae01364b8755aa2acae116420e59104
End Point <ahmedmohamed23345@gmail.com>
2020-04-05 15:41:55 +0200

    ch

--- app/src/main/java/com/developer/enjad/activities_fragments/activity_edit_report/EditReportActivity.java
+++ app/src/main/java/com/developer/enjad/activities_fragments/activity_edit_report/EditReportActivity.java
@@ -1,16 +1,90 @@
 package com.developer.enjad.activities_fragments.activity_edit_report;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.GridLayoutManager;
+
 
 import com.developer.enjad.R;
+import com.developer.enjad.adapter.ReportsAdapter;
+import com.developer.enjad.databinding.ActivityEditReportBinding;
+import com.developer.enjad.interfaces.Listeners;
+import com.developer.enjad.language.LanguageHelper;
+import com.developer.enjad.models.Reports_Model;
 
-public class EditReportActivity extends AppCompatActivity {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import io.paperdb.Paper;
+
+public class EditReportActivity extends AppCompatActivity implements Listeners.BackListener {
+    private ActivityEditReportBinding binding;
+
+    private String lang;
+private List<Reports_Model.Data> data;
+private ReportsAdapter reportsAdapter;
+    private Reports_Model reports_model;
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        Paper.init(newBase);
+        super.attachBaseContext(LanguageHelper.updateResources(newBase, Paper.book().read("lang", "ar")));
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_report);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_report);
+        initView();
+
     }
+
+
+
+    @SuppressLint("RestrictedApi")
+    private void initView() {
+        binding.toolbar.setTitle("");
+        data = new ArrayList<>();
+        reportsAdapter = new ReportsAdapter( this,data);
+        Paper.init(this);
+        lang = Paper.book().read("lang", Locale.getDefault().getLanguage());binding.setLang(lang);
+        binding.setBackListener(this);
+        binding.recView.setLayoutManager(new GridLayoutManager(this, 1));
+        binding.recView.setAdapter(reportsAdapter);
+getdata();
+
+
+    }
+
+    private void getdata() {
+      if(getIntent().getSerializableExtra("data")!=null){
+           reports_model= (Reports_Model) getIntent().getSerializableExtra("data");
+           if(reports_model.getData()!=null&&reports_model.getData().size()>0){
+               data.clear();
+               data.addAll(reports_model.getData());
+           }
+      }
+reportsAdapter.notifyDataSetChanged();
+        }
+
+
+
+    @Override
+    public void back() {
+        finish();
+    }
+
+
 }
---
+++ app/src/main/java/com/developer/enjad/adapter/ReportsAdapter.java
@@ -0,0 +1,100 @@
+package com.developer.enjad.adapter;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.developer.enjad.R;
+import com.developer.enjad.databinding.ReportRow2Binding;
+import com.developer.enjad.databinding.ReportRowBinding;
+import com.developer.enjad.models.Reports_Model;
+
+
+import java.util.List;
+
+public class ReportsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final int ITEM_DATA = 1;
+    private final int ITEM_DATA2 = 2;
+
+    private List<Reports_Model.Data> dataList;
+    private Context context;
+    public ReportsAdapter(Context context, List<Reports_Model.Data> dataList) {
+
+        this.dataList = dataList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if (viewType == ITEM_DATA) {
+            ReportRowBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.report_row,parent,false);
+            return new MyHolder(binding);
+        } else {
+            ReportRow2Binding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.report_row2,parent,false);
+            return new MyHolder2(binding);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, int position) {
+
+        if (holder instanceof MyHolder) {
+
+            MyHolder myHolder = (MyHolder) holder;
+            myHolder.binding.setReportsmodel(dataList.get(position));
+
+        } else {
+            MyHolder2 myHolder2 = (MyHolder2) holder;
+            myHolder2.binding.setReportsmodel(dataList.get(position));
+
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+        private ReportRowBinding binding;
+        public MyHolder(ReportRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
+        }
+
+
+    }
+
+    public class MyHolder2 extends RecyclerView.ViewHolder {
+
+        private ReportRow2Binding binding;
+        public MyHolder2(ReportRow2Binding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (position%2 == 0) {
+            return ITEM_DATA2;
+        } else {
+            return ITEM_DATA;
+
+        }
+
+
+    }
+}
